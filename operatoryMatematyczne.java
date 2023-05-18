86295541cd5244dfe188af242495332134c12860
AdrianStarzynski <adrian.starzynski.it@gmail.com>
2023-05-18 21:42:06 +0200

    kalkulator

--- operatoryMatematyczne.java
+++ operatoryMatematyczne.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-public class operatoryMatematyczne {
-
-    public static void main(String[] args) {
-        int firstNumber = 4;
-        int secondNumber =6;
-        float thirdNumber = 4.0F;
-
-
-        int addition = firstNumber + secondNumber; //10
-        int subtraction = firstNumber - secondNumber; //-2
-        int multiplication = firstNumber * secondNumber; //24
-        float division = thirdNumber/secondNumber; //0,66
-        int mod = firstNumber%secondNumber; //ile 4 miesci sie w 6 = 1,2
-
-        System.out.println("Wynik dodawania= " + addition) ;
-        System.out.println("Wynik odejmowania= " + subtraction) ;
-        System.out.println("Wynik mnozenia= " + multiplication) ;
-        System.out.println("Wynik dzielenia= " + division) ;
-        System.out.println("Wynik modulo= " +  mod) ;
-    }}
-
-
-
-
-
-
-
-
-
-=======
 public class operatoryMatematyczne {
 
     public static void main(String[] args) {
@@ -39,7 +8,7 @@ public class operatoryMatematyczne {
         int addition = firstNumber + secondNumber; //10
         int subtraction = firstNumber - secondNumber; //-2
         int multiplication = firstNumber * secondNumber; //24
-        int division = firstNumber / secondNumber; //0,66
+        int division = firstNumber/secondNumber; //0,66
         int mod = firstNumber%secondNumber; //ile 4 mieśći się w 6 = 1,2
 
         System.out.println("Wynik dodawania" + addition) ;
@@ -51,14 +20,3 @@ public class operatoryMatematyczne {
 
 
 
-
-
-
-
-
-
-
-
-
-
->>>>>>> aa71d97 (changes)
